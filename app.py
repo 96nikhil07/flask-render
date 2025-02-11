d1d6c3cfc411b86b12d3da7deec864fd2fd39c66
Pawar <NPAWAR8@jaguarlandrover.com>
2025-02-11 15:49:28 +0530

    fourth commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
+'''
 if __name__ == '__main__':
     app.run("0.0.0.0",5000)
-
+'''
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
